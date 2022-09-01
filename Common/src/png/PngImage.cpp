62c925ee20040932495be36fa41cbebb4a3c0b73
jozef17 <j.blazicek17@gmail.com>
2022-09-01 14:10:27 +0200

    Added Average filter and Refactored logging

--- Common/src/png/PngImage.cpp
+++ Common/src/png/PngImage.cpp
@@ -7,11 +7,12 @@
 #include "Common.hpp"
 
 #include <memory>
-
-#include <iostream>
 #include <cstdint>
+
+#ifdef ENABLE_LOGS
+#include <iostream>
 #include <iomanip>
-#include <bitset>
+#endif
 
 #define TO_INT(data)  ((data[0] << 8 * 3) | (data[1] << 8 * 2) | (data[2] << 8 * 1) | data[3])
 
@@ -46,7 +47,7 @@ PngImage::PngImage(const std::string& filename)
 	}
 
 	// Check if png data
-	uint8_t signature[8];
+	uint8_t signature[8] = { 0 };
 	file.read((char*)signature, sizeof(signature));
 	uint8_t reference[8] = { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
 	if (std::memcmp(signature, reference, 8) != 0)
@@ -65,25 +66,10 @@ PngImage::PngImage(const std::string& filename)
 		}
 		else if (chunk->header.chunkType == "IDAT")
 		{
-			///////////////////////////////////////////////////////////////////////////////////////////////
-			std::cout << "------------------------------------------------------------" << std::endl;//////
-			std::cout << "IDAT" << std::endl;//////////////////////////////////////////////////////////////
-			for (int i = 0; i < chunk->header.lenght; i++)/////////////////////////////////////////////////
-			{//////////////////////////////////////////////////////////////////////////////////////////////
-				auto byte = chunk->data[i];////////////////////////////////////////////////////////////////
-				for (int j = 0; j < 8; j++)////////////////////////////////////////////////////////////////
-				{//////////////////////////////////////////////////////////////////////////////////////////
-					auto bit = byte & 1 << j;//////////////////////////////////////////////////////////////
-					std::cout << (bit > 0 ? "1" : "0");////////////////////////////////////////////////////
-				}//////////////////////////////////////////////////////////////////////////////////////////
-			}//////////////////////////////////////////////////////////////////////////////////////////////
-			std::cout << std::endl;////////////////////////////////////////////////////////////////////////
-			std::cout << "------------------------------------------------------------" << std::endl;//////
-			/////////////////////////////////////////////////////////////////////////////////////////////*/
 			bitstream.Append(std::move(chunk->data), chunk->header.lenght);
 		}
 
-		// TODO handle other chunks if needed
+		// Handle other chunks if needed
 
 		else if (chunk->header.chunkType == "IEND")
 		{
@@ -100,45 +86,52 @@ std::unique_ptr<Chunk> PngImage::LoadChnuk(std::ifstream& file)
 	std::unique_ptr<Chunk> chunk = std::make_unique<Chunk>();
 
 	// Read size
-	uint8_t data[4];
+	uint8_t data[4] = { 0 };
 	file.read((char*)data, 4);
 	chunk->header.lenght = TO_INT(data);
-	/*///////////////////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << "Size: " << std::dec << chunk->header.lenght << std::endl;//////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////*/
+#ifdef ENABLE_LOGS
+	std::cout << "[PngImage::LoadChnuk] - Size: " << std::dec << chunk->header.lenght << std::endl;
+#endif
 
 	// Read chunk type
 	file.read((char*)data, 4);
 	chunk->header.chunkType = std::string((char*)data, 4);
-	/*///////////////////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << "Type: " << chunk->header.chunkType << std::endl;///////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////*/
+#ifdef ENABLE_LOGS
+	std::cout << "                      - Type: " << chunk->header.chunkType << std::endl;
+#endif
 
 	// Read data
 	chunk->data = std::unique_ptr<uint8_t[]>(new uint8_t[chunk->header.lenght]);
 	file.read((char*)chunk->data.get(), chunk->header.lenght);
-	/*////////////////////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << "Data: ";///////////////////////////////////////////////////////////////////////////////////
-	for (uint32_t i = 0; i < (chunk->header.lenght < 100 ? chunk->header.lenght : 100); i++)/////////////////
-	{////////////////////////////////////////////////////////////////////////////////////////////////////////
-		std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)chunk->data[i] << " ";////////////
-	}////////////////////////////////////////////////////////////////////////////////////////////////////////
-	if (chunk->header.lenght > 100)//////////////////////////////////////////////////////////////////////////
-		std::cout << "...";//////////////////////////////////////////////////////////////////////////////////
-	std::cout << std::endl;//////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////*/
+#ifdef ENABLE_LOGS
+	std::cout << "                      - Data: ";
+	for (uint32_t i = 0; i < (chunk->header.lenght < 100 ? chunk->header.lenght : 100); i++)
+	{
+		for (uint32_t j = 0; j < 8; j++)
+		{
+			auto bit = chunk->data[i] & 1 << j;
+			std::cout << (bit > 0 ? "1" : "0");
+		}
+		std::cout << " ";
+	}
+
+	if (chunk->header.lenght > 100)
+	{
+		std::cout << "...";
+	}
+	std::cout << std::endl;	
+#endif
 
 	// Read crc
 	file.read((char*)&chunk->crc, 4);
-	/*////////////////////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << "Crc : ";///////////////////////////////////////////////////////////////////////////////////
-	for (int i = 0; i < 4; i++)//////////////////////////////////////////////////////////////////////////////
-	{////////////////////////////////////////////////////////////////////////////////////////////////////////
-		std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)chunk->crc[i] << " ";//////////////
-	}////////////////////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << std::endl;//////////////////////////////////////////////////////////////////////////////////
-	std::cout << std::endl;//////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////*/
+#ifdef ENABLE_LOGS
+	std::cout << "                      - Crc: ";
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)chunk->crc[i] << " ";
+	}
+	std::cout << std::endl;
+#endif
 	return std::move(chunk);
 }
 
@@ -147,22 +140,32 @@ void PngImage::ProcessHeader(std::unique_ptr<Chunk> &ihdrChunk)
 	IHDR ihdr = { 0 };
 	std::memcpy(&ihdr, ihdrChunk->data.get(), sizeof(IHDR));
 
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << ihdrChunk->header.chunkType << std::endl;/////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << "- width:             ";//////////////////////////////////////////////////////////////////////////////////////////////////////////
-	for (int i = 0; i < 4; i++)////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)ihdr.width[i] << " ";///////////////////////////////////////////////////
-	std::cout << " (" << std::dec << (int)(TO_INT(ihdr.width)) << ") ";		///////////////////////////////////////////////////////////////////////
-	std::cout << " [" << std::dec << (int)(this->width) << "]" << std::endl;		///////////////////////////////////////////////////////////////
-	std::cout << "- height:            ";//////////////////////////////////////////////////////////////////////////////////////////////////////////
-	for (int i = 0; i < 4; i++)////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)ihdr.height[i] << " ";//////////////////////////////////////////////////
-	std::cout << " (" << std::dec << (int)(TO_INT(ihdr.height)) << ") ";///////////////////////////////////////////////////////////////////////////
-	std::cout << " [" << std::dec << (int)(this->height) << "]" << std::endl;//////////////////////////////////////////////////////////////////////
-	std::cout << "- chanellSize:       " << std::setw(2) << std::setfill('0') << std::hex << (int)ihdr.chanellSize << std::endl;///////////////////
-	std::cout << "- colorType:         " << std::setw(2) << std::setfill('0') << std::hex << (int)ihdr.colorType << std::endl;/////////////////////
-	std::cout << "- compressionMethod: " << std::setw(2) << std::setfill('0') << std::hex << (int)ihdr.compressionMethod << std::endl;/////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifdef ENABLE_LOGS
+	std::cout << "[PngImage::ProcessHeader] " << ihdrChunk->header.chunkType << std::endl;
+
+	std::cout << "                          - width:  ";
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)ihdr.width[i] << " ";
+	}
+	std::cout << " (" << std::dec << (int)(TO_INT(ihdr.width)) << ") ";
+	std::cout << " [" << std::dec << (int)(this->width) << "]" << std::endl;
+
+	std::cout << "                          - height: ";
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)ihdr.height[i] << " ";
+	}
+	std::cout << " (" << std::dec << (int)(TO_INT(ihdr.height)) << ") ";
+	std::cout << " [" << std::dec << (int)(this->height) << "]" << std::endl;
+
+	std::cout << "                          - chanellSize:       " 
+		<< std::setw(2) << std::setfill('0') << std::hex << (int)ihdr.chanellSize << std::endl;
+	std::cout << "                          - colorType:         " 
+		<< std::setw(2) << std::setfill('0') << std::hex << (int)ihdr.colorType << std::endl;
+	std::cout << "                          - compressionMethod: " 
+		<< std::setw(2) << std::setfill('0') << std::hex << (int)ihdr.compressionMethod << std::endl;
+#endif
 
 	// Check unsupported types
 	// NOTE: only RGB & RGBA supported
@@ -206,32 +209,31 @@ void PngImage::ProcessData(BitStream& bitstream)
 {
 	Inflate decoder(bitstream);
 	auto decodedBytes = decoder.Decode();
+	auto bytesPerPixel = static_cast<ColorType>(this->colorType) == ColorType::TrueColor ? 3 : 4;
 
-	///////////////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << std::endl << "DATA:" << std::endl;	///////////////////////////////////////////////////
-	for (int a = 0; a < decodedBytes.size(); a++)	///////////////////////////////////////////////////
-	{//////////////////////////////////////////////////////////////////////////////////////////////////
-		std::cout << std::setw(3) << std::setfill('0') << std::dec << (int)decodedBytes.at(a) << " ";//
-		if (a % (4 * width + 1) == 0)//////////////////////////////////////////////////////////////////
-			std::cout << " ";//////////////////////////////////////////////////////////////////////////
-		if (a % (4 * width + 1) == (4 * width))////////////////////////////////////////////////////////
-			std::cout << std::endl;////////////////////////////////////////////////////////////////////
-	}//////////////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << std::endl;////////////////////////////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////////////////////////////////////*/
+#ifdef ENABLE_LOGS
+	std::cout << "[PngImage::ProcessData] Decoded Data:" << std::endl;
+	for (int a = 0; a < decodedBytes.size(); a++)
+	{
+		std::cout << std::setw(3) << std::setfill('0') << std::dec << (int)decodedBytes.at(a) << " ";
+		if (a % (bytesPerPixel * width + 1) == (bytesPerPixel * width))
+		{
+			std::cout << std::endl;
+		}
+	}
+	std::cout << std::endl;
+#endif
 
 	this->image = std::unique_ptr<std::unique_ptr<Pixel>[]>(new std::unique_ptr<Pixel>[this->width * this->height]);
 
 	auto start = 0;
-	auto bytesPerPixel = static_cast<ColorType>(this->colorType) == ColorType::TrueColor ? 3 : 4;
 	while (decodedBytes.size() < this->width * this->height * bytesPerPixel + this->height)
 	{
-//		std::cout << "???? " << (int)decodedBytes.at(start) << std::endl;
 		decodedBytes.push_back(decodedBytes.at(start++));
 	}
-	//////////////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << std::endl << "Pixels:" << std::endl;	//////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////////////////////////////////////
+#ifdef ENABLE_LOGS
+	std::cout << "[PngImage::ProcessData] Pixels:" << std::endl;
+#endif
 
 	// "Defilter" data
 	auto loc = 0;
@@ -249,7 +251,6 @@ void PngImage::ProcessData(BitStream& bitstream)
 				alpha = decodedBytes.at(loc++);
 			}
 
-			// TODO
 			switch (filterType)
 			{
 			case 0:
@@ -286,29 +287,45 @@ void PngImage::ProcessData(BitStream& bitstream)
 				break;
 			}
 			case 3:
-				std::cout << "average filter" << std::endl;
-				// TODO average
+			{
+				// Average
+				RGBAPixel leftPixel = { 0,0,0,0 }; // a
+				if (j > 0)
+				{
+					auto left = i * this->width + j - 1;
+					leftPixel = this->image[left].get()->ToRGBA();
+				}
+
+				auto above = (i - 1) * this->width + j; // b
+				auto abovePixel = this->image[above].get()->ToRGBA();
+
+
+				red   = static_cast<uint8_t>(red   + std::floor(abovePixel.red   + leftPixel.red)   / 2);
+				green = static_cast<uint8_t>(green + std::floor(abovePixel.green + leftPixel.green) / 2);
+				blue  = static_cast<uint8_t>(blue  + std::floor(abovePixel.blue  + leftPixel.blue)  / 2);
+				alpha = static_cast<uint8_t>(alpha + std::floor(abovePixel.alpha + leftPixel.alpha) / 2);
+
 				break;
+			}
+
 			case 4:
-				std::cout << "peath filter" << std::endl;
-				// TODO peath
+				throw RuntimeException("peath filter not Implemented!!!!");
 				break;
 			default:
 				throw RuntimeException("Invalid filtering method: "+std::to_string(filterType));
 			}
-			//////////////////////////////////////////////////////////////////////////////////////////////////
-			std::cout << std::setw(3) << std::setfill('0') << std::dec << (int)red << " ";////////////////////
-			std::cout << std::setw(3) << std::setfill('0') << std::dec << (int)green << " ";//////////////////
-			std::cout << std::setw(3) << std::setfill('0') << std::dec << (int)blue << " ";///////////////////
-			std::cout << std::setw(3) << std::setfill('0') << std::dec << (int)alpha << " ";//////////////////
-			std::cout << "   ";///////////////////////////////////////////////////////////////////////////////
-			//////////////////////////////////////////////////////////////////////////////////////////////////
-
+#ifdef ENABLE_LOGS
+			std::cout << std::setw(3) << std::setfill('0') << std::dec << (int)red << " ";
+			std::cout << std::setw(3) << std::setfill('0') << std::dec << (int)green << " ";
+			std::cout << std::setw(3) << std::setfill('0') << std::dec << (int)blue << " ";
+			std::cout << std::setw(3) << std::setfill('0') << std::dec << (int)alpha << " ";
+			std::cout << ", ";
+#endif
 			RGBAPixel rgba{ red, green, blue, alpha };
 			this->image[i * this->width + j] = std::unique_ptr<Pixel>(new Pixel(rgba));
 		}
-		//////////////////////////////////////////////////////////////////////////////////////////////////
-		std::cout << std::endl;	//////////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////////////////////////////
-	} //*/
+#ifdef ENABLE_LOGS
+		std::cout << std::endl;
+#endif
+	}
 }
